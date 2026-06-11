546feef6d2910644e4eb0ebc44dcb95c6f5c4419
Nafisa Tabassum <nafisa.tabassum53@qmail.cuny.edu>
2026-06-11 15:57:50 -0400

    Add cart item count update functionality
    Added functionality to update cart count when items are added.

--- NafisaTabassum_exam-1-frontend/script.js
+++ NafisaTabassum_exam-1-frontend/script.js
@@ -1,9 +1,13 @@
 const productCards = document.querySelectorAll(".product-card");
+const cartCount = document.querySelector(".cart-count");
+
+let totalCartItems = 0;
 
 productCards.forEach(function (card) {
     const minusButton = card.querySelector(".minus-btn");
     const plusButton = card.querySelector(".plus-btn");
     const quantityText = card.querySelector(".quantity");
+    const addToCartButton = card.querySelector(".cart-btn");
 
     // Increase quantity Functionality
     plusButton.addEventListener("click", function () {
@@ -20,5 +24,13 @@ productCards.forEach(function (card) {
             currentQuantity--;
             quantityText.textContent = currentQuantity;
         }
+    });
+
+    // Add selected quantity to cart count
+    addToCartButton.addEventListener("click", function () {
+        let currentQuantity = Number(quantityText.textContent);
+
+        totalCartItems = totalCartItems + currentQuantity;
+        cartCount.textContent = totalCartItems;
     });
 });
